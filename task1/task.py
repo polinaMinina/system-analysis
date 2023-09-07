fc6dd772bd6b9a5569c9223daf4b65fb85a2d077
Polina Minina <115030427+polinaMinina@users.noreply.github.com>
2023-09-07 11:55:46 +0300

    Update task.py

--- task1/task.py
+++ task1/task.py
@@ -1,1 +1,15 @@
+import csv
 
+i = 5
+j = 3
+
+with open('file.csv', 'r', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    for _ in range(i - 1):
+        next(csvreader)
+    
+    row = next(csvreader)
+    element = row[j - 1]
+
+    print(f'На позиции ({i}, {j}): {element}')
